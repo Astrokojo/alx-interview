e0c8801bff057cf7dce3e2728366ef58d5aa7240
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-06-19 04:08:09 +0000

    feat: add 0-making_change.py a0-main.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script to solve the coin change problem"""
+
+def makeChange(coins, total):
+    """Script to solve the coin change problem"""
+    subs = [total + 1] * (total + 1)
+    subs[0] = 0
+    for i in range(1, total + 1):
+        for j in range(0, len(coins)):
+            if coins[j] <= i:
+                subs[i] = min(subs[i], subs[i - coins[j]] + 1)
+                
+    return -1 if subs[total] > total else subs[total]
+            
+   
